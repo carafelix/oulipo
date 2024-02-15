a51315ffa57e58a44497a4bf525de9e9fb79b7fb
Hero Protagonist <heroprotagonist32@gmail.com>
2024-02-15 12:58:56 -0300

    broked by a mile

--- main.py
+++ main.py
@@ -1,56 +1,84 @@
-# -*- tab-width: 4; indent-tabs-mode: nil; py-indent-offset: 4 -*-
-#
-# This file is part of the LibreOffice project.
-#
-# This Source Code Form is subject to the terms of the Mozilla Public
-# License, v. 2.0. If a copy of the MPL was not distributed with this
-# file, You can obtain one at http://mozilla.org/MPL/2.0/.
-#
-
-import sys
+import uno
 import unohelper
-import officehelper
-
 from com.sun.star.task import XJobExecutor
+from com.sun.star.awt import XKeyHandler
+from com.sun.star.awt import Key
+from com.sun.star.text import XTextDocument
+from com.sun.star.lang import XEventListener
+from com.sun.star.awt.MessageBoxButtons import BUTTONS_OK
+from com.sun.star.awt.MessageBoxType import INFOBOX
+
+ 
+class Wavelet( unohelper.Base, XJobExecutor, XEventListener ):
+    def __init__( self, ctx ):
+        self.ctx = ctx
+
+        desktop = ctx.getServiceManager().createInstanceWithContext("com.sun.star.frame.Desktop", ctx)
+        self.doc = desktop.getCurrentComponent()
+
+        self.doc.Text.addEventListener(wavelet)
 
 
-# The MainJob is a UNO component derived from unohelper.Base class
-# and also the XJobExecutor, the implemented interface
-class MainJob(unohelper.Base, XJobExecutor):
-    def __init__(self, context):
-        self.context = context
-        # handling different situations (inside LibreOffice or other process)
+    def documentEventOccured(self, event):
+        if event.EventName == "InsertText":
+            self.trigger(None)
+
+    def get_cursor_on_last_modified_word(self):
+        if self.doc is None:
+            return
+        
+        text = self.doc.Text
+
+        # Get the view cursor
+        view_cursor = self.doc.CurrentController.getViewCursor()
+        
+        # Get the position of the last modified character
+
+        last_modified_position = view_cursor.getStart()
+
+        # Get the word at the last modified position
+
+        cursor = text.createTextCursorByRange(last_modified_position)
+        cursor.gotoStartOfWord(False)
+        cursor.gotoEndOfWord(True)
+        last_modified_word = cursor.getString()
+        
+        return cursor
+    
+    def trigger( self, args ):
+        if self.doc is None:
+            return
+
+        last_word_cursor = self.get_cursor_on_last_modified_word(doc)
+
         try:
-            self.sm = context.getServiceManager()
-            self.desktop = XSCRIPTCONTEXT.getDesktop()
-        except NameError:
-            self.sm = context.ServiceManager
-            self.desktop = self.context.getServiceManager().createInstanceWithContext(
-                "com.sun.star.frame.Desktop", self.context)
+            search = self.doc.createSearchDescriptor()
+            search.SearchWords = True
+            search.SearchString = last_word_cursor.getString()
 
-    def trigger(self, args):
-        desktop = self.context.ServiceManager.createInstanceWithContext(
-            "com.sun.star.frame.Desktop", self.context)
-        model = desktop.getCurrentComponent()
-        if not hasattr(model, "Text"):
-            model = self.desktop.loadComponentFromURL("private:factory/swriter", "_blank", 0, ())
-        text = model.Text
-        cursor = text.createTextCursor()
-        text.insertString(cursor, "Hello fukin fokers -> " + args + "\n", 0)
-
+            found = self.doc.findAll( search )
+            
+            if(found.Count > 1):
+                last_word_cursor.CharBackColor = 0xFFFF00
+                # for i in range(found.Count):
+                #     found.getByIndex(i).CharBackColor = 0xFFFF00
+            else:
+                # since we are only selecting tangent words, this doesn't get updated when you erase the duplicate word
+                last_word_cursor.CharBackColor = -1
+    
+        except Error as e:
+            show_message_box(e, "Error")
+            pass
 
 # Starting from Python IDE
 def main():
     try:
-        context = XSCRIPTCONTEXT
+        ctx = XSCRIPTCONTEXT
     except NameError:
-        context = officehelper.bootstrap()
-        if context is None:
+        ctx = officehelper.bootstrap()
+        if ctx is None:
             print("ERROR: Could not bootstrap default Office.")
             sys.exit(1)
-    job = MainJob(context)
-    job.trigger()
-
 
 # Starting from command line
 if __name__ == "__main__":
@@ -59,10 +87,19 @@ if __name__ == "__main__":
 
 # pythonloader loads a static g_ImplementationHelper variable
 g_ImplementationHelper = unohelper.ImplementationHelper()
-
 g_ImplementationHelper.addImplementation(
-    MainJob,  # UNO object class
+    Wavelet,  # UNO object class
     "org.extension.sample.do",  # implementation name (customize for yourself)
     ("com.sun.star.task.Job",), )  # implemented services (only 1)
 
-# vim: set shiftwidth=4 softtabstop=4 expandtab:
+    
+
+
+
+
+# def MessageBox(ParentWindow, MsgText, MsgTitle, MsgType, MsgButtons):
+#     ctx = XSCRIPTCONTEXT.getComponentContext()
+#     sm = ctx.ServiceManager
+#     si = sm.createInstanceWithContext("com.sun.star.awt.Toolkit", ctx)
+#     mBox = si.createMessageBox(ParentWindow, MsgType, MsgButtons, MsgTitle, MsgText)
+#     mBox.execute()
